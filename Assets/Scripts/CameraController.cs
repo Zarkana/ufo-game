4b0d95706652cff749af887303b766d2c05b68b6
Zarkana <jsc940@gmail.com>
2016-11-19 11:33:24 -1000

    lesson 5

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+  public GameObject player;
+
+  private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+    //transform references the transform of the object that this script is attached to
+    offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+  {
+    transform.position = player.transform.position + offset;
+	}
+}
