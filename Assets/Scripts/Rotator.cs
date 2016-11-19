e90f0fa940e627b069817bf8808471eead7304bb
Zarkana <jsc940@gmail.com>
2016-11-19 12:22:26 -1000

    lesson 7

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+  
+
+  //Use Update when not using forces
+	void Update ()
+  {
+    transform.Rotate(new Vector3(0,0,45) * Time.deltaTime);
+	}
+}
